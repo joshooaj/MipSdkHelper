63558615269657aa4d56500ff12d2c71927e5723
Josh Hendricks <jhendricks123@gmail.com>
2018-05-23 09:15:35 -0700

    Add project files.

---
+++ MipSdkHelper/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MipSdkHelper
+{
+    public class Class1
+    {
+    }
+}
